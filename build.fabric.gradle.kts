bcf3136017e46755e9809bf0ba144811e6937141
Cassian <cassian@godsted.com>
2026-01-07 12:05:13 -0500

    Tag fixes and 26.1-snapshot-2

--- build.fabric.gradle.kts
+++ build.fabric.gradle.kts
@@ -116,10 +116,10 @@ publishMods {
     file = tasks.remapJar.map { it.archiveFile.get() }
     additionalFiles.from(tasks.remapSourcesJar.map { it.archiveFile.get() })
 
-    type = BETA
+    type = STABLE
     displayName = "${property("mod.name")} ${property("mod.version")} for ${stonecutter.current.version} Fabric"
     version = "${property("mod.version")}+${property("deps.minecraft")}-fabric"
-    changelog = provider { rootProject.file("CHANGELOG.md").readText() }
+    changelog = provider { rootProject.file("CHANGELOG-LATEST.md").readText() }
     modLoaders.add("fabric")
 
     modrinth {
