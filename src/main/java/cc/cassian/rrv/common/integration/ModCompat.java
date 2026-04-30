a3c7f7ab22b35626897ed76bd0a13a5cca9475b8
Cassian <cassian@godsted.com>
2026-04-30 13:19:14 -0400

    fix polymer mod loaded check

--- src/main/java/cc/cassian/rrv/common/integration/ModCompat.java
+++ src/main/java/cc/cassian/rrv/common/integration/ModCompat.java
@@ -4,7 +4,7 @@ import cc.cassian.rrv.common.Platform;
 
 public class ModCompat {
 	public static final boolean POLYDEX = Platform.INSTANCE.isModLoaded("polydex");
-	public static final boolean POLYMER = Platform.INSTANCE.isModLoaded("polymer-common");
+	public static final boolean POLYMER = Platform.INSTANCE.isModLoaded("polymer-bundled");
 	public static final boolean ITEM_DESCRIPTIONS = Platform.INSTANCE.isModLoaded("item_descriptions");
 	public static final boolean JADE = Platform.INSTANCE.isModLoaded("jade");
 	public static final boolean WTHIT = Platform.INSTANCE.isModLoaded("wthit");
