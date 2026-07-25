3d1ef058d3b0842c3c2eb6abb374768cb57d9e81
Cassian <cassian@godsted.com>
2026-07-24 21:35:00 -0400

    De-duplicate overlay code

--- src/main/java/cc/cassian/rrv/common/mixin/integration/jei/JeiRecipesGuiMixin.java
+++ src/main/java/cc/cassian/rrv/common/mixin/integration/jei/JeiRecipesGuiMixin.java
@@ -1,15 +1,11 @@
 package cc.cassian.rrv.common.mixin.integration.jei;
 
-import cc.cassian.rrv.api.ActionType;
-import cc.cassian.rrv.client.ReliableRecipeViewerClient;
 import cc.cassian.rrv.client.util.RRVClientUtil;
 import cc.cassian.rrv.client.util.RRVExtendedContainerScreen;
 import cc.cassian.rrv.common.config.Configs;
 import cc.cassian.rrv.common.overlay.AbstractRrvOverlay;
 import cc.cassian.rrv.common.overlay.BlockingGuiComponent;
 import cc.cassian.rrv.common.overlay.OverlayManager;
-import cc.cassian.rrv.common.overlay.itemlist.bookmark.BookmarkManager;
-import cc.cassian.rrv.common.overlay.itemlist.view.ItemViewOverlay;
 import cc.cassian.rrv.common.recipe.inventory.RecipeViewScreen;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -18,10 +14,6 @@ import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.common.util.ImmutableRect2i;
 import mezz.jei.gui.recipes.RecipesGui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.components.Renderable;
-import net.minecraft.client.gui.components.events.GuiEventListener;
-import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.client.input.MouseButtonEvent;
@@ -30,7 +22,6 @@ import net.minecraft.world.item.ItemStack;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -80,36 +71,15 @@ public abstract class JeiRecipesGuiMixin extends Screen implements RRVExtendedCo
 				info.imageHeight()
 		));
 		OverlayManager.INSTANCE.setCurrentInvInfo(info);
-		this.updateWidgets();
+		RRVExtendedContainerScreen.updateWidgets(this);
 	}
 
 	@Inject(method = "extractRenderState", at = @At("TAIL"))
 	private void injectOverlay$1(GuiGraphicsExtractor guiGraphics, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
 		if (minecraft == null || Configs.CLIENT_SETTINGS.isJeiPanel()) return;
-
-
 		AbstractRrvOverlay.InventoryPositionInfo info = new AbstractRrvOverlay.InventoryPositionInfo(this, this.width, this.height, this.area.x(), this.area.y(), this.area.width(), this.area.height());
-
-		OverlayManager.INSTANCE.setGuiBlocking(new BlockingGuiComponent(
-				RRVClientUtil.CONTAINER,
-				info.leftPos(),
-				info.topPos(),
-				info.imageWidth(),
-				info.imageHeight()
-		));
-
-		if (OverlayManager.INSTANCE.checkForScreenChange(info)) {
-			OverlayManager.INSTANCE.updateOverlaysAndWidgets(false);
-		}
-
-		if (OverlayManager.INSTANCE.hasQueuedWidgetUpdate())
-			this.updateWidgets();
-
-
-		OverlayManager.INSTANCE.renderAll(guiGraphics, mouseX, mouseY, partialTicks);
-
+		RRVExtendedContainerScreen.extractOverlay(info, guiGraphics, mouseX, mouseY, partialTicks);
 	}
-
 
 	@Inject(method = "mouseScrolled", at = @At("TAIL"), cancellable = true)
 	private void injectOverlay$2(double mouseX, double mouseY, double scrolledX, double scrolledY, CallbackInfoReturnable<Boolean> cir) {
@@ -118,41 +88,16 @@ public abstract class JeiRecipesGuiMixin extends Screen implements RRVExtendedCo
 			cir.setReturnValue(true);
 	}
 
-
 	@Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
 	private void injectOverlay$3(KeyEvent keyEvent, CallbackInfoReturnable<Boolean> cir) {
-		if (minecraft == null || Configs.CLIENT_SETTINGS.isJeiPanel()) return;
-
-		if (OverlayManager.INSTANCE.isTextWidgetFocused() && this.getFocused() instanceof EditBox box) {
-			box.keyPressed(keyEvent);
-
-			if (!keyEvent.isEscape() && !keyEvent.isCycleFocus())
-				cir.setReturnValue(true);
-
-			return;
-		}
-
-
-		if (OverlayManager.INSTANCE.keyPressed(keyEvent))
+		var pressed = handleKeyPress(this, keyEvent);
+		if (pressed) {
 			cir.setReturnValue(true);
-
-		if (this.hoveredSlot() == null)
-			return;
-
-		if (ReliableRecipeViewerClient.USAGE_KEYBIND.matches(keyEvent))
-			ItemViewOverlay.INSTANCE.openRecipeView(this.hoveredSlot(), ActionType.INPUT);
-
-		if (ReliableRecipeViewerClient.RECIPE_KEYBIND.matches(keyEvent))
-			ItemViewOverlay.INSTANCE.openRecipeView(this.hoveredSlot(), ActionType.RESULT);
-
-		if (ReliableRecipeViewerClient.ADD_BOOKMARK_KEYBIND.matches(keyEvent)) {
-			BookmarkManager.INSTANCE.bookmarkItem(this.hoveredSlot());
-
 		}
 	}
 
-	@Unique
-	private ItemStack hoveredSlot() {
+	@Override
+	public ItemStack rrv$hoveredStack() {
 		return getIngredientUnderMouse(VanillaTypes.ITEM_STACK).orElse(null);
 	}
 
@@ -161,33 +106,9 @@ public abstract class JeiRecipesGuiMixin extends Screen implements RRVExtendedCo
 		return super.mouseClicked(mouseButtonEvent, b) | OverlayManager.INSTANCE.mouseClicked(mouseButtonEvent, b);
 	}
 
-	@Inject(method = "onClose", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "onClose", at = @At("HEAD"))
 	private void injectOverlay$4(CallbackInfo ci) {
-		OverlayManager.INSTANCE.oldWidgets().clear();
-		OverlayManager.INSTANCE.screenContextMap().clear();
-
-	}
-
-
-	@Unique
-	private void updateWidgets() {
-		if (minecraft == null || Configs.CLIENT_SETTINGS.isJeiPanel()) return;
-		OverlayManager.INSTANCE.oldWidgets().forEach(eventListener -> {
-
-			if (eventListener.isFocused())
-				this.setFocused(null);
-
-			this.removeWidget(eventListener);
-		});
-		OverlayManager.INSTANCE.oldWidgets().clear();
-
-		OverlayManager.INSTANCE.screenContextMap().forEach((abstractRrvOverlay, screenContext) -> {
-			screenContext.renderables().forEach(eventListener -> this.addRenderableWidget((GuiEventListener & Renderable & NarratableEntry) eventListener));
-			screenContext.nonRenderables().forEach(eventListener -> this.addWidget((GuiEventListener & NarratableEntry) eventListener));
-		});
-
-		OverlayManager.INSTANCE.setQueuedWidgetUpdate(false);
-
+		RRVExtendedContainerScreen.clearOverlay();
 	}
 
 	@Override
